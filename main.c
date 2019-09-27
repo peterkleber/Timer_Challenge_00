883a2eebd8c3fb4be31a610b9e12dbf28b2a2e6a
peterkleber <38935088+peterkleber@users.noreply.github.com>
2019-09-27 22:31:54 +0200

    Dummy App to test 7Seg Driver

--- main.c
+++ main.c
@@ -9,10 +9,14 @@
 #include <util/delay.h>
 #include "DIO_cnfg.h"
 #include "DIO.h"
+#include "Seven_Seg_Driver.h"
+
 
 int main() {
 
 	DIO_init(LED0);
+
+	Seven_Seg_Init();
 
 	while (1) {
 
@@ -20,6 +24,11 @@ int main() {
 		_delay_ms(500); // delay 500 ms
 		DIO_write(PORT_B, PIN4, LOW, LED0); //turn off LED0
 		_delay_ms(500); //delay 500 ms
+
+		for(uint8 i = 0 ; i < 10 ; i++){
+			Seven_Seg_Write(i);
+			_delay_ms(500);
+		}
 
 	}
 }
