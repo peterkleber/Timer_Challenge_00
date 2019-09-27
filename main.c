59c90aa894d6b4cda301213fd28225e0e42c570f
peterkleber <38935088+peterkleber@users.noreply.github.com>
2019-09-27 22:26:16 +0200

    Dummy App to test DIO Driver

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 /*
- * main.c
+ * DIO_LINK_cnfg.c
  *
- * Created: 9/20/2019
- *  Author: Peter Kleber
+ * Created: 9/13/2019 10:22:43 AM
+ * Author : Peter
  */
+
+#include "Registers.h"
+#include <util/delay.h>
+#include "DIO_cnfg.h"
+#include "DIO.h"
 
 int main() {
 
+	DIO_init(LED0);
+
 	while (1) {
+
+		DIO_write(PORT_B, PIN4, HIGH, LED0); //turn on LED0
+		_delay_ms(500); // delay 500 ms
+		DIO_write(PORT_B, PIN4, LOW, LED0); //turn off LED0
+		_delay_ms(500); //delay 500 ms
 
 	}
 }
